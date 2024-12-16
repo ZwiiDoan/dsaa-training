2b17dea9c7723b0fdec94ec4c749ea27ecde8c88
zwiidoan <zwii.doan@gmail.com>
2024-12-16 21:51:29 +1100

    Leetcode DS&A: Bonus Problems Iteration 5 - upto Binary Trees BFS

--- src/main/java/per/duyd/training/dsaa/treesandgraphs/BinaryTrees.java
+++ src/main/java/per/duyd/training/dsaa/treesandgraphs/BinaryTrees.java
@@ -2,8 +2,12 @@ package per.duyd.training.dsaa.treesandgraphs;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
+import java.util.Stack;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class BinaryTrees {
@@ -83,6 +87,116 @@ public class BinaryTrees {
     }
   }
 
+  public boolean isSymmetric(TreeNode root) {
+    if (root == null) {
+      return false;
+    }
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+      int levelSize = queue.size();
+      TreeNode[] levelNodes = new TreeNode[levelSize];
+
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode node = queue.poll();
+
+        if (node != null) {
+          queue.offer(node.left);
+          queue.offer(node.right);
+        }
+
+        levelNodes[i] = node;
+      }
+
+      for (int i = 0, j = levelSize - 1; i < j; i++, j--) {
+        if (levelNodes[i] == levelNodes[j]) {
+          continue;
+        } else if (levelNodes[i] == null || levelNodes[j] == null) {
+          return false;
+        } else if (levelNodes[i].val != levelNodes[j].val) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+    List<List<Integer>> resultPaths = new ArrayList<>();
+    if (root == null) {
+      return resultPaths;
+    }
+
+    Stack<NodeAndPath> treeStack = new Stack<>();
+    treeStack.push(new NodeAndPath(root, List.of(root.val), root.val));
+
+    while (!treeStack.isEmpty()) {
+      NodeAndPath nodeAndPath = treeStack.pop();
+
+      if (nodeAndPath.pathSum == targetSum) {
+        if (nodeAndPath.treeNode.left == null && nodeAndPath.treeNode.right == null) {
+          resultPaths.add(new ArrayList<>(nodeAndPath.path));
+        }
+      }
+
+      if (nodeAndPath.treeNode.right != null) {
+        List<Integer> newPath = new ArrayList<>(nodeAndPath.path);
+        newPath.add(nodeAndPath.treeNode.right.val);
+        treeStack.push(new NodeAndPath(nodeAndPath.treeNode.right, newPath,
+            nodeAndPath.pathSum + nodeAndPath.treeNode.right.val));
+      }
+
+      if (nodeAndPath.treeNode.left != null) {
+        List<Integer> newPath = new ArrayList<>(nodeAndPath.path);
+        newPath.add(nodeAndPath.treeNode.left.val);
+        treeStack.push(new NodeAndPath(nodeAndPath.treeNode.left, newPath,
+            nodeAndPath.pathSum + nodeAndPath.treeNode.left.val));
+      }
+    }
+
+    return resultPaths;
+  }
+
+  public List<List<Integer>> pathSumBackTracking(TreeNode root, int targetSum) {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    backtrack(root, targetSum, path, result);
+    return result;
+  }
+
+  private void backtrack(TreeNode node, int remainingSum, List<Integer> path,
+                         List<List<Integer>> result) {
+    if (node == null) {
+      return;
+    }
+
+    path.add(node.val);
+
+    if (node.left == null && node.right == null && remainingSum == node.val) {
+      result.add(new ArrayList<>(path));
+    } else {
+      backtrack(node.left, remainingSum - node.val, path, result);
+      backtrack(node.right, remainingSum - node.val, path, result);
+    }
+
+    path.remove(path.size() - 1); // backtrack
+  }
+
+  private static class NodeAndPath {
+    TreeNode treeNode;
+    List<Integer> path;
+    int pathSum;
+
+    public NodeAndPath(TreeNode treeNode, List<Integer> path, int pathSum) {
+      this.treeNode = treeNode;
+      this.path = path;
+      this.pathSum = pathSum;
+    }
+  }
+
   public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
     List<Integer> elements1 = new ArrayList<>();
     getAllElements(root1, elements1);
@@ -122,5 +236,347 @@ public class BinaryTrees {
       elements.add(node.val);
       getAllElements(node.right, elements);
     }
+  }
+
+  public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+    if (root1 == null && root2 == null) {
+      return true;
+    }
+
+    List<Integer> leafSequence1 = new ArrayList<>();
+    List<Integer> leafSequence2 = new ArrayList<>();
+
+    getLeaf(root1, leafSequence1);
+    getLeaf(root2, leafSequence2);
+
+    return leafSequence1.equals(leafSequence2);
+  }
+
+  private void getLeaf(TreeNode node, List<Integer> leafSequence) {
+//    if (node != null) {
+//      if (node.left != null) {
+//        getLeaf(node.left, leafSequence);
+//      }
+//
+//      if (node.right != null) {
+//        getLeaf(node.right, leafSequence);
+//      }
+//
+//      if (node.left == null && node.right == null) {
+//        leafSequence.add(node.val);
+//      }
+//    }
+    Stack<TreeNode> stack = new Stack<>();
+    stack.push(node);
+
+    while (!stack.isEmpty()) {
+      TreeNode currentNode = stack.pop();
+
+      if (currentNode == null) {
+        continue;
+      }
+
+      if (currentNode.right != null) {
+        stack.push(currentNode.right);
+      }
+
+      if (currentNode.left != null) {
+        stack.push(currentNode.left);
+      }
+
+      if (currentNode.left == null && currentNode.right == null) {
+        leafSequence.add(currentNode.val);
+      }
+    }
+  }
+
+  public TreeNode invertTree(TreeNode root) {
+    if (root == null) {
+      return null;
+    }
+
+    TreeNode tmp = root.right;
+    root.right = invertTree(root.left);
+    root.left = invertTree(tmp);
+
+    return root;
+  }
+
+  public TreeNode removeLeafNodes(TreeNode root, int target) {
+    if (root == null) {
+      return null;
+    }
+
+    root.left = removeLeafNodes(root.left, target);
+    root.right = removeLeafNodes(root.right, target);
+
+    if (root.left == null && root.right == null && root.val == target) {
+      return null;
+    } else {
+      return root;
+    }
+  }
+
+  public int pathSumCount(TreeNode root, int targetSum) {
+    Map<Long, Integer> preSumMap = new HashMap<>();
+    return countPathsWithTargetSum(root, 0, targetSum, preSumMap);
+  }
+
+  private int countPathsWithTargetSum(TreeNode root, long previousSum, int targetSum,
+                                      Map<Long, Integer> preSumMap) {
+    if (root == null) {
+      return 0;
+    }
+
+    int count = 0;
+    long currentSum = root.val + previousSum;
+
+    if (currentSum == targetSum) {
+      count++;
+    }
+
+    count += preSumMap.getOrDefault(currentSum - targetSum, 0);
+
+    preSumMap.put(currentSum, preSumMap.getOrDefault(currentSum, 0) + 1);
+
+    count += countPathsWithTargetSum(root.left, currentSum, targetSum, preSumMap);
+    count += countPathsWithTargetSum(root.right, currentSum, targetSum, preSumMap);
+
+    preSumMap.put(currentSum, preSumMap.getOrDefault(currentSum, 0) - 1);
+
+    return count;
+  }
+
+  public int pathSumCountIterative(TreeNode root, int targetSum) {
+    if (root == null) {
+      return 0;
+    }
+
+    int count = 0;
+    Stack<SumNode> stack = new Stack<>();
+    stack.push(new SumNode(root, 0, new HashMap<>()));
+
+    while (!stack.isEmpty()) {
+      SumNode sumNode = stack.pop();
+      TreeNode currentNode = sumNode.node;
+      long previousSum = sumNode.previousSum;
+      Map<Long, Integer> previousMap = sumNode.preSumMap;
+
+      if (currentNode == null) {
+        continue;
+      }
+
+      long currentSum = previousSum + currentNode.val;
+
+      if (currentSum == targetSum) {
+        count++;
+      }
+
+      count += previousMap.getOrDefault(currentSum - targetSum, 0);
+
+      Map<Long, Integer> currentMap = new HashMap<>(previousMap);
+      currentMap.put(currentSum, previousMap.getOrDefault(currentSum, 0) + 1);
+
+      stack.push(new SumNode(currentNode.right, currentSum, currentMap));
+      stack.push(new SumNode(currentNode.left, currentSum, currentMap));
+    }
+
+    return count;
+  }
+
+  record SumNode(TreeNode node, long previousSum, Map<Long, Integer> preSumMap) {
+  }
+
+  public static final boolean DIRECTION_LEFT = true;
+  public static final boolean DIRECTION_RIGHT = false;
+
+  public int longestZigZag(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+
+    return Math.max(longestZigZag(root.left, 0, DIRECTION_LEFT),
+        longestZigZag(root.right, 0, DIRECTION_RIGHT));
+  }
+
+  private int longestZigZag(TreeNode currentNode, int currentLength, boolean currentDirection) {
+    if (currentNode == null) {
+      return currentLength;
+    }
+
+    currentLength++;
+
+    return Math.max(
+        longestZigZag(currentNode.left, currentDirection == DIRECTION_RIGHT ? currentLength : 0,
+            DIRECTION_LEFT),
+        longestZigZag(currentNode.right, currentDirection == DIRECTION_LEFT ? currentLength : 0,
+            DIRECTION_RIGHT));
+  }
+
+  public int longestZigZagIterative(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int maxZigZagLength = 0;
+
+    Stack<ZigZagNode> stack = new Stack<>();
+    stack.push(new ZigZagNode(root.right, 0, DIRECTION_RIGHT));
+    stack.push(new ZigZagNode(root.left, 0, DIRECTION_LEFT));
+
+    while (!stack.isEmpty()) {
+      ZigZagNode currentNode = stack.pop();
+      if (currentNode.node == null) {
+        maxZigZagLength = Math.max(maxZigZagLength, currentNode.currentLength);
+      } else {
+        stack.push(new ZigZagNode(currentNode.node.right,
+            currentNode.currentDirection == DIRECTION_LEFT ? currentNode.currentLength + 1 : 0,
+            DIRECTION_RIGHT));
+        stack.push(new ZigZagNode(currentNode.node.left,
+            currentNode.currentDirection == DIRECTION_RIGHT ? currentNode.currentLength + 1 : 0,
+            DIRECTION_LEFT));
+      }
+    }
+
+    return maxZigZagLength;
+  }
+
+  record ZigZagNode(TreeNode node, int currentLength, boolean currentDirection) {
+  }
+
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    List<List<Integer>> ans = new ArrayList<>();
+    Queue<TreeNode> queue = new ArrayDeque<>();
+    if (root == null) {
+      return ans;
+    }
+
+    queue.offer(root);
+    while (!queue.isEmpty()) {
+      List<Integer> levelNodes = new ArrayList<>();
+      int levelSize = queue.size();
+
+      while (levelSize > 0) {
+        TreeNode node = queue.poll();
+        levelSize--;
+        levelNodes.add(node.val);
+
+        if (node.left != null) {
+          queue.offer(node.left);
+        }
+
+        if (node.right != null) {
+          queue.offer(node.right);
+        }
+      }
+
+      ans.add(levelNodes);
+    }
+
+    return ans;
+  }
+
+  public int maxLevelSum(TreeNode root) {
+    Queue<TreeNode> queue = new ArrayDeque<>();
+    queue.offer(root);
+
+    int maxSum = Integer.MIN_VALUE;
+    int maxLevel = 1;
+    int currLevel = 0;
+
+    while (!queue.isEmpty()) {
+      currLevel++;
+      int levelSize = queue.size();
+      int levelSum = 0;
+
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode node = queue.poll();
+        levelSum += node.val;
+
+        if (node.left != null) {
+          queue.offer(node.left);
+        }
+
+        if (node.right != null) {
+          queue.offer(node.right);
+        }
+      }
+
+      if (levelSum > maxSum) {
+        maxSum = levelSum;
+        maxLevel = currLevel;
+      }
+    }
+
+    return maxLevel;
+  }
+
+  public List<Double> averageOfLevels(TreeNode root) {
+    Queue<TreeNode> queue = new ArrayDeque<>();
+    List<Double> levelAverages = new ArrayList<>();
+
+    if (root == null) {
+      return List.of();
+    }
+
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+      int levelSize = queue.size();
+      double levelSum = 0;
+
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode treeNode = queue.poll();
+        levelSum += treeNode.val;
+
+        if (treeNode.left != null) {
+          queue.offer(treeNode.left);
+        }
+        if (treeNode.right != null) {
+          queue.offer(treeNode.right);
+        }
+      }
+
+      levelAverages.add(levelSum / levelSize);
+    }
+
+    return levelAverages;
+  }
+
+  public boolean isEvenOddTree(TreeNode root) {
+    if (root == null) {
+      return false;
+    }
+
+    Queue<TreeNode> queue = new ArrayDeque<>();
+    queue.offer(root);
+    boolean isEvenLevel = true;
+
+    while (!queue.isEmpty()) {
+      int levelSize = queue.size();
+
+      int previousValue = isEvenLevel ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode treeNode = queue.poll();
+        if (isEvenLevel && (previousValue >= treeNode.val || treeNode.val % 2 == 0)) {
+          return false;
+        } else if (!isEvenLevel && (previousValue <= treeNode.val || treeNode.val % 2 != 0)) {
+          return false;
+        } else {
+          previousValue = treeNode.val;
+        }
+
+        if (treeNode.left != null) {
+          queue.offer(treeNode.left);
+        }
+        if (treeNode.right != null) {
+          queue.offer(treeNode.right);
+        }
+      }
+
+      isEvenLevel = !isEvenLevel;
+    }
+
+    return true;
   }
 }
--- src/test/java/per/duyd/training/dsaa/treesandgraphs/BinaryTreesTest.java
+++ src/test/java/per/duyd/training/dsaa/treesandgraphs/BinaryTreesTest.java
@@ -32,6 +32,224 @@ class BinaryTreesTest {
             new TreeNode(8, new TreeNode(1), null), List.of(1, 1, 8, 8)));
   }
 
+  public static Stream<Arguments> leafSimilarParams() {
+    return Stream.of(Arguments.of("Example 1", new TreeNode(3,
+            new TreeNode(5, new TreeNode(6), new TreeNode(2, new TreeNode(7), new TreeNode(4))),
+            new TreeNode(1, new TreeNode(9), new TreeNode(8))),
+        new TreeNode(3, new TreeNode(5, new TreeNode(6), new TreeNode(7)),
+            new TreeNode(1, new TreeNode(4), new TreeNode(2, new TreeNode(9), new TreeNode(8)))),
+        true), Arguments.of("Example 2", new TreeNode(1, new TreeNode(2), new TreeNode(3)),
+        new TreeNode(1, new TreeNode(3), new TreeNode(2)), false));
+  }
+
+  public static Stream<Arguments> invertTreeParams() {
+    return Stream.of(Arguments.of("Example 1",
+            new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+                new TreeNode(7, new TreeNode(6), new TreeNode(9))),
+            new TreeNode(4, new TreeNode(7, new TreeNode(9), new TreeNode(6)),
+                new TreeNode(2, new TreeNode(3), new TreeNode(1)))),
+        Arguments.of("Example 2", new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+            new TreeNode(2, new TreeNode(3), new TreeNode(1))));
+  }
+
+  public static Stream<Arguments> isSymmetricParams() {
+    return Stream.of(Arguments.of("Example 1",
+        new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+            new TreeNode(2, new TreeNode(4), new TreeNode(3))), true), Arguments.of("Example 2",
+        new TreeNode(1, new TreeNode(2, null, new TreeNode(3)),
+            new TreeNode(2, null, new TreeNode(3))), false));
+  }
+
+  public static Stream<Arguments> pathSumParams() {
+    return Stream.of(
+        Arguments.of("Example 1", new TreeNode(5,
+                new TreeNode(4,
+                    new TreeNode(11,
+                        new TreeNode(7), new TreeNode(2)),
+                    null),
+                new TreeNode(8,
+                    new TreeNode(13),
+                    new TreeNode(4,
+                        new TreeNode(5),
+                        new TreeNode(1)))),
+            22,
+            List.of(List.of(5, 4, 11, 2), List.of(5, 8, 4, 5))
+        ),
+        Arguments.of("Example 2", new TreeNode(1, new TreeNode(2), new TreeNode(3)), 5, List.of())
+    );
+  }
+
+  public static Stream<Arguments> removeLeafNodesParams() {
+    return Stream.of(
+        Arguments.of("Example 1",
+            new TreeNode(1,
+                new TreeNode(2, new TreeNode(2), null),
+                new TreeNode(3, new TreeNode(2), new TreeNode(4))),
+            2,
+            new TreeNode(1, null, new TreeNode(3, null, new TreeNode(4)))),
+        Arguments.of("Example 2",
+            new TreeNode(1,
+                new TreeNode(3, new TreeNode(3), new TreeNode(2)),
+                new TreeNode(3)),
+            3,
+            new TreeNode(1, new TreeNode(3, null, new TreeNode(2)), null)),
+        Arguments.of("Example 3",
+            new TreeNode(1, new TreeNode(2, new TreeNode(2, new TreeNode(2), null), null), null), 2,
+            new TreeNode(1)
+        )
+    );
+  }
+
+  public static Stream<Arguments> pathSumCountParams() {
+    return Stream.of(
+        Arguments.of("Example 1", new TreeNode(10,
+                new TreeNode(5,
+                    new TreeNode(3, new TreeNode(3), new TreeNode(-2)),
+                    new TreeNode(2, null, new TreeNode(1))),
+                new TreeNode(-3, null, new TreeNode(11))),
+            8,
+            3
+        ),
+        Arguments.of("Example 2", new TreeNode(5,
+                new TreeNode(4,
+                    new TreeNode(11, new TreeNode(7), new TreeNode(2)),
+                    null),
+                new TreeNode(8,
+                    new TreeNode(13, new TreeNode(5), new TreeNode(1)),
+                    new TreeNode(4))),
+            22,
+            3
+        ),
+        Arguments.of("Example 3", new TreeNode(1), 0, 0)
+    );
+  }
+
+  public static Stream<Arguments> longestZigZagParams() {
+    return Stream.of(
+        Arguments.of(
+            "Example 1",
+            new TreeNode(1,
+                null,
+                new TreeNode(1,
+                    new TreeNode(1),
+                    new TreeNode(1,
+                        new TreeNode(1,
+                            null,
+                            new TreeNode(1, null, new TreeNode(1))),
+                        new TreeNode(1)
+                    )
+                )
+            ), 3
+        ),
+        Arguments.of(
+            "Example 2",
+            new TreeNode(1,
+                new TreeNode(1,
+                    null,
+                    new TreeNode(1,
+                        new TreeNode(1,
+                            null,
+                            new TreeNode(1)),
+                        new TreeNode(1))),
+                new TreeNode(1)
+            ), 4
+        )
+    );
+  }
+
+  public static Stream<Arguments> levelOrderParams() {
+    return Stream.of(
+        Arguments.of(
+            "Example 1",
+            new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20,
+                    new TreeNode(15),
+                    new TreeNode(7)
+                )
+            ), List.of(List.of(3), List.of(9, 20), List.of(15, 7))
+        ),
+        Arguments.of(
+            "Example 2",
+            new TreeNode(1),
+            List.of(List.of(1))
+        ),
+        Arguments.of(
+            "Example 3",
+            null,
+            List.of()
+        )
+    );
+  }
+
+  public static Stream<Arguments> maxLevelSumParams() {
+    return Stream.of(
+        Arguments.of("Example 1",
+            new TreeNode(1,
+                new TreeNode(7, new TreeNode(7), new TreeNode(-8)),
+                new TreeNode(0)
+            ), 2
+        ),
+        Arguments.of("Example 2",
+            new TreeNode(989,
+                null,
+                new TreeNode(10250,
+                    new TreeNode(98693),
+                    new TreeNode(-89388, null, new TreeNode(-32127)))
+            ), 2
+        )
+    );
+  }
+
+  public static Stream<Arguments> averageOfLevelsParams() {
+    return Stream.of(
+        Arguments.of(
+            "Example 1",
+            new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20, new TreeNode(15), new TreeNode(7))),
+            List.of(3d, 14.5d, 11d)
+        ),
+        Arguments.of(
+            "Example 2",
+            new TreeNode(3,
+                new TreeNode(9, new TreeNode(15), new TreeNode(7)),
+                new TreeNode(20)),
+            List.of(3d, 14.5d, 11d)
+        )
+    );
+  }
+
+  public static Stream<Arguments> isEvenOddTreeParams() {
+    return Stream.of(
+        Arguments.of(
+            "Example 1",
+            new TreeNode(1,
+                new TreeNode(10,
+                    new TreeNode(3, new TreeNode(12), new TreeNode(8)),
+                    null),
+                new TreeNode(4,
+                    new TreeNode(7, new TreeNode(6), null),
+                    new TreeNode(9, null, new TreeNode(2)))),
+            true
+        ),
+        Arguments.of(
+            "Example 2",
+            new TreeNode(5,
+                new TreeNode(4, new TreeNode(3), new TreeNode(3)),
+                new TreeNode(2, new TreeNode(7), null)),
+            false
+        ),
+        Arguments.of(
+            "Example 3",
+            new TreeNode(5,
+                new TreeNode(9, new TreeNode(3), new TreeNode(5)),
+                new TreeNode(1, new TreeNode(7), null)),
+            false
+        )
+    );
+  }
+
   @BeforeEach
   void setUp() {
     binaryTrees = new BinaryTrees();
@@ -89,5 +307,74 @@ class BinaryTreesTest {
   @MethodSource("getAllElementsParams")
   void getAllElements(String description, TreeNode root1, TreeNode root2, List<Integer> expected) {
     assertIterableEquals(expected, binaryTrees.getAllElements(root1, root2));
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("leafSimilarParams")
+  void leafSimilar(String description, TreeNode root1, TreeNode root2, boolean expected) {
+    assertEquals(expected, binaryTrees.leafSimilar(root1, root2));
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("invertTreeParams")
+  void invertTree(String description, TreeNode root, TreeNode expected) {
+    assertTrue(binaryTrees.isSameTree(expected, binaryTrees.invertTree(root)));
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("isSymmetricParams")
+  void isSymmetric(String description, TreeNode root, boolean expected) {
+    assertEquals(expected, binaryTrees.isSymmetric(root));
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("pathSumParams")
+  void pathSum(String description, TreeNode root, int targetSum, List<List<Integer>> expected) {
+    assertIterableEquals(expected, binaryTrees.pathSum(root, targetSum));
+    assertIterableEquals(expected, binaryTrees.pathSumBackTracking(root, targetSum));
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("removeLeafNodesParams")
+  void removeLeafNodes(String description, TreeNode root, int target, TreeNode expected) {
+    assertTrue(binaryTrees.isSameTree(expected, binaryTrees.removeLeafNodes(root, target)));
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("pathSumCountParams")
+  void pathSumCount(String description, TreeNode root, int targetSum, int expected) {
+    assertEquals(expected, binaryTrees.pathSumCount(root, targetSum));
+    assertEquals(expected, binaryTrees.pathSumCountIterative(root, targetSum));
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("longestZigZagParams")
+  void longestZigZag(String description, TreeNode root, int expected) {
+    assertEquals(expected, binaryTrees.longestZigZag(root));
+    assertEquals(expected, binaryTrees.longestZigZagIterative(root));
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("levelOrderParams")
+  void levelOrder(String description, TreeNode root, List<List<Integer>> expected) {
+    assertIterableEquals(expected, binaryTrees.levelOrder(root));
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("maxLevelSumParams")
+  void maxLevelSum(String description, TreeNode root, int expected) {
+    assertEquals(expected, binaryTrees.maxLevelSum(root));
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("averageOfLevelsParams")
+  void averageOfLevels(String description, TreeNode root, List<Double> expected) {
+    assertIterableEquals(expected, binaryTrees.averageOfLevels(root));
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("isEvenOddTreeParams")
+  void isEvenOddTree(String description, TreeNode root, boolean expected) {
+    assertEquals(expected, binaryTrees.isEvenOddTree(root));
   }
 }
